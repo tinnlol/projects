5d531033725e8eb8f3ee0475924c36f5d3a798aa
w3irddy <118191333+w3irddy@users.noreply.github.com>
2023-01-31 16:32:11 +0300

    Create HWIDWhitelist.lua

---
+++ HWIDWhitelist.lua
@@ -0,0 +1,12 @@
+local allowedhwids = loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/configs/main/HWIDWhitelist.lua", true))()
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+
+
+for i,v in pairs(allowedhwids) do
+    if hwid == allowedhwids[i] then
+        warn("whitelisted!")
+        return
+    elseif i == #allowedhwids then
+        game:GetService("Players").LocalPlayer.kick(game:GetService("Players").LocalPlayer, "You are not whitelisted.")
+    end
+end
