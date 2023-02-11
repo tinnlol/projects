8922a89af20a372c2bfc39afc28a544e5104be2d
w3irddy <118191333+w3irddy@users.noreply.github.com>
2023-02-11 19:49:10 +0300

    Update whitelistcheck.lua

--- whitelistcheck.lua
+++ whitelistcheck.lua
@@ -1,10 +1,1 @@
-local hwids = loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPrivate/main/WhitelistedUsers.lua", true))()
-local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
 
-for i,v in pairs(hwids) do
-    if hwid == v and #hwids ~= 0 then
-        return true;
-    elseif hwid ~= v and i == #hwids and #hwids ~= 0 then
-        return false;
-    end
-end
